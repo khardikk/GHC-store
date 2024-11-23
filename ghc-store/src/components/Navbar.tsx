0404b6ea23dbc4782bf8aefadafa58e53032e79d
hardikk kamboj <kambojpushkal@gmail.com>
2024-11-23 23:11:47 +0530

    Fixed logo

--- ghc-store/src/components/Navbar.tsx
+++ ghc-store/src/components/Navbar.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-
+import Logo from '../../public/logo.png';
 const Navbar: React.FC = () => {
   return (
     <nav className="flex justify-between items-center py-4 px-6 bg-white z-10 relative">
       <div className="flex items-center">
-        <h1 className="text-3xl font-bold mr-2">General Habit Store</h1>
-        <span className="text-xl">By Good Habits Club</span>
+        <img src={Logo} />
       </div>
       <div>
         <h4 className="text-lg font-medium">CART (4)</h4>
