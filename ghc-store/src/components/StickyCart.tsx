4da3f36e8d7c1cde1bd873cbc7674d0fc6ba4675
hardikk kamboj <kambojpushkal@gmail.com>
2024-11-24 19:11:38 +0530

    Fixed design

--- ghc-store/src/components/StickyCart.tsx
+++ ghc-store/src/components/StickyCart.tsx
@@ -12,7 +12,7 @@ const StickyCart: React.FC = () => {
 
   return (
     <div className="fixed bottom-0 left-0 right-0 p-4 z-50 bg-slate-50">
-      <div className="bg-[#4339F2] rounded-2xl px-6 py-4 flex items-center justify-between max-w-[1400px] mx-auto border border-black/10">
+      <div className="bg-[#4339F2] rounded-2xl px-6 py-4 flex items-center justify-between  mx-auto border border-black/10">
         <div className="flex items-center gap-4 text-white">
           <span className="font-medium">
             {totalItems} item{totalItems > 1 ? 's' : ''}
