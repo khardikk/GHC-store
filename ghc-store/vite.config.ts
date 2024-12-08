a187dbc252902d9c4878d554cff3e8d13597e2da
hardikk kamboj <kambojpushkal@gmail.com>
2024-12-08 21:12:32 +0530

    deployment fix

--- ghc-store/vite.config.ts
+++ ghc-store/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://ghc-store-backend-production.up.railway.app',
         changeOrigin: true
       }
     }
